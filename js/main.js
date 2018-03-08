52770c89660e85addb1be04de7ecbe7bda75f81b
Matt <mtounkara90@gmail.com>
2018-03-08 13:03:51 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -8,13 +8,12 @@ $(document).ready(function() {
         $("#theMessage").animate({left: '200px'}, "slow");
         $("#theMessage").fadeOut(2000);
         $("#theMessage").fadeIn(2000);
-        $("#theMessage").animate({right: '400px'}, "slow");
-
-        $("#theMessage").fadeOut(2000, function(){
-          // our last fadeOut is done, let's center it
-          $('#theMessage').attr('style', '');
-          $("#theMessage").fadeIn(2000);
-
-        });
+        $("#theMessage").animate({left: '-10px'}, "slow");
+        $("#theMessage").fadeOut(2000);
+        $("#theMessage").fadeIn(2000);
 
 });
+        // $("#theMessage").fadeOut(2000, function(){
+          // our last fadeOut is done, let's center it
+          // $('#theMessage').attr('style', '');
+        // });
